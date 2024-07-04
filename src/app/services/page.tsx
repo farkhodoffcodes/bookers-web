f84bad95797218e777d5ad50655cda782cac924a
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-07-04 16:42:38 +0500

    100000000%%%%%

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,15 +1,144 @@
 import ServicesCard from "@/components/cards/services-card"
 import { Metadata } from "next";
+import images from '@/assets/serveces/servece.png'
+import avatar from '@/assets/serveces/avatar.png'
 
 export const metadata: Metadata = {
   title: "Bookers | Services",
   description: "Bookers website for services",
 };
 
+const servicesData = [
+  {
+    id: 1,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 2,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 3,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 4,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 5,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 6,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 7,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 8,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 9,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+  {
+    id: 10,
+    mainImage: images,
+    title: 'Женская для взрослых',
+    avatarImage: avatar,
+    firstName: 'Натали',
+    service: 'Женский мастер',
+    price: '500 000',
+    location: 'Мирабадский р-н, ул. Нурафшон, 32',
+    posts: 'Ближайшая запись: Сегодня',
+    buttonName: 'Записаться'
+  },
+]
+
 const Services = () => {
   return (
-    <section>
-      <ServicesCard />
+    <section className="py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {servicesData.map(services => (
+          <ServicesCard data={services} />
+        ))}
+      </div>
     </section>
   )
 }
--- src/components/cards/services-card.tsx
+++ src/components/cards/services-card.tsx
@@ -1,40 +1,39 @@
 import React from 'react';
 import Image from 'next/image';
 import { FaMapMarkerAlt } from 'react-icons/fa';
+import Button from '../button/button';
 
-const ServicesCard = () => {
-  return (
-    <div className="max-w-sm mx-auto rounded-lg shadow-lg bg-gray-100 p-6">
-      <Image 
-        src="" 
-        alt="Barber shop"
-        className="rounded-lg w-full mb-4"
-      />
-      <div>
-        <h3 className="text-xl font-bold mb-2">Женская для взрослых</h3>
-        <div className="flex items-center mb-4">
-          <Image 
-            src="" 
-            alt="Nathalie"
-            className="w-10 h-10 rounded-full mr-3"
-          />
-          <div>
-            <p className="text-lg font-semibold">Натали</p>
-            <p className="text-sm text-gray-600">Женский мастер</p>
-          </div>
+const ServicesCard = ({ data }: { data: any }) => {
+    return (
+        <div className="rounded-xl shadow-lg bg-[#B9B9C9] p-6">
+            <Image
+                src={data.mainImage}
+                alt="Barber shop"
+                className="rounded-lg w-full mb-4"
+            />
+            <div>
+                <h3 className="text-xl font-bold mb-2">{data.title}</h3>
+                <div className="flex items-center mb-4">
+                    <Image
+                        src={data.avatarImage}
+                        alt="Nathalie"
+                        className="w-10 h-10 rounded-full mr-3"
+                    />
+                    <div>
+                        <p className="text-lg font-semibold">{data.firstName}</p>
+                        <p className="text-sm text-gray-600">{data.service}</p>
+                    </div>
+                </div>
+                <p className="text-2xl font-bold text-red-600 mb-4">{data.price} сум</p>
+                <div className="flex items-center mb-4">
+                    <FaMapMarkerAlt className="text-blue-500" />
+                    <p className="text-sm text-gray-700 ml-2">{data.location}</p>
+                </div>
+                <p className="text-sm text-gray-700 mb-4">{data.posts}</p>
+                <Button title={data.buttonName} />
+            </div>
         </div>
-        <p className="text-2xl font-bold text-red-600 mb-4">350 000 сум</p>
-        <div className="flex items-center mb-4">
-          <FaMapMarkerAlt className="text-blue-500" />
-          <p className="text-sm text-gray-700 ml-2">Миpабадский р-н, ул. Нурафшон, 32</p>
-        </div>
-        <p className="text-sm text-gray-700 mb-4">Ближайшая запись: Сегодня</p>
-        <button className="w-full bg-red-600 text-white py-2 rounded-lg hover:bg-red-700">
-          Записаться
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ServicesCard;
