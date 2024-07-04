7b954c7c89e7f9b4adaf05906283bcc81b24b08f
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-07-04 16:02:50 +0500

    button update

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Button: React.FC<ButtonTypes> = ({ title, onClick, isDisabled = false, outlineStyle, icon }) => {
   return (
-    <button onClick={onClick} disabled={isDisabled} className={outlineStyle ? '' : 'bg-[#9C0B35] border-[1px] border-[#9C0B35] text-white rounded-3xl py-2 px-7 w-[100%] active:opacity-[.9] '}>{title}</button>
+    <button onClick={onClick} disabled={isDisabled} className={`${outlineStyle ? 'bg-transparent text-[#9C0B35] border-[1px] border-[#9C0B35] rounded-3xl py-2 px-7 w-[100%] active:opacity-[.9]' : 'bg-[#9C0B35] text-white border-[1px] border-[#9C0B35] rounded-3xl py-2 px-7 w-[100%] active:opacity-[.9]'}`}>{title}</button>
   )
 }
 
