90fdfd3af4750e19494dabed077fc2774d949095
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-07-05 18:12:08 +0500

    ....

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,11 +1,30 @@
 "use client"
 
-import { Metadata } from "next";
 import ServicesCard from "@/components/cards/services-card";
+import { Metadata } from "next";
 import images from "@/assets/serveces/servece.png";
 import avatar from "@/assets/serveces/avatar.png";
 import Hero from "@/components/hero/hero";
 import opacha from "../../../assets/images/firstHero.png";
+
+import i18n from "i18next";
+import { useTranslation } from "next-i18next";
+import { initReactI18next } from "react-i18next";
+import { en } from "@/cons/language/en";
+import { ru } from "@/cons/language/ru";
+import { uz } from "@/cons/language/uz";
+import languageStore from "@/types/language/languageStore";
+import { useEffect } from "react";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    ru: { translation: ru },
+    uz: { translation: uz },
+  },
+  lng: "ru",
+  fallbackLng: "ru",
+});
 
 export const metadata: Metadata = {
   title: "Bookers | Services",
@@ -136,6 +155,14 @@ const servicesData = [
 ];
 
 const Services = () => {
+  const { selectedLanguage } = languageStore();
+  const { t } = useTranslation();
+
+  // Component ilk render bo'lganda ishlaydigan useEffect
+  useEffect(() => {
+    i18n.changeLanguage(selectedLanguage);
+  }, [selectedLanguage]);
+
   return (
     <section className="py-10 relative">
       <div className="-translate-y-20">
@@ -146,7 +173,7 @@ const Services = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {servicesData.map((services) => (
-          <ServicesCard data={services} key={services.id} />
+          <ServicesCard key={services.id} data={services} />
         ))}
       </div>
     </section>
