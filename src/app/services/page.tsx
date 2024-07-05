ef230416019ce54b765650abbf1d788a25b9c83b
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-07-05 17:59:51 +0500

    buldiyov

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,5 @@
-"use client"
-import ServicesCard from "@/components/cards/services-card";
 import { Metadata } from "next";
+import ServicesCard from "@/components/cards/services-card";
 import images from "@/assets/serveces/servece.png";
 import avatar from "@/assets/serveces/avatar.png";
 import Hero from "@/components/hero/hero";
@@ -8,22 +7,22 @@ import opacha from "../../../assets/images/firstHero.png";
 
 import i18n from "i18next";
 import { useTranslation } from "next-i18next";
-import { initReactI18next } from "react-i18next";
-import { en } from "@/cons/language/en";
-import { ru } from "@/cons/language/ru";
-import { uz } from "@/cons/language/uz";
+// import { initReactI18next } from "react-i18next";
+// import { en } from "@/cons/language/en";
+// import { ru } from "@/cons/language/ru";
+// import { uz } from "@/cons/language/uz";
 import languageStore from "@/types/language/languageStore";
 import { useEffect } from "react";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    ru: { translation: ru },
-    uz: { translation: uz },
-  },
-  lng: "ru",
-  fallbackLng: "ru",
-});
+// i18n.use(initReactI18next).init({
+//   resources: {
+//     en: { translation: en },
+//     ru: { translation: ru },
+//     uz: { translation: uz },
+//   },
+//   lng: "ru",
+//   fallbackLng: "ru",
+// });
 
 export const metadata: Metadata = {
   title: "Bookers | Services",
@@ -154,13 +153,13 @@ const servicesData = [
 ];
 
 const Services = () => {
-  const { selectedLanguage } = languageStore();
-  const { t } = useTranslation();
-
-  // Component ilk render bo'lganda ishlaydigan useEffect
-  useEffect(() => {
-    i18n.changeLanguage(selectedLanguage);
-  }, [selectedLanguage]);
+  // const { selectedLanguage } = languageStore();
+  // const { t } = useTranslation();
+  //
+  // // Component ilk render bo'lganda ishlaydigan useEffect
+  // useEffect(() => {
+  //   i18n.changeLanguage(selectedLanguage);
+  // }, [selectedLanguage]);
 
   return (
     <section className="py-10 relative">
